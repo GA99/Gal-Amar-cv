1959388dbc2ab2e444592dd6a4359529566de241
Gal amar <61208357+GA99@users.noreply.github.com>
2022-05-31 18:38:38 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/** @format */
+
+function leaveBody() {
+	// const leave = document.getElementsByTagName('html');
+	document.getElementById('leaveBG').style.display = 'block';
+}
+function closebtn() {
+	document.getElementById('leaveBG').style.display = 'none';
+}
